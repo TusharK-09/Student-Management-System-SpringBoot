9f6a87c4d4a1d43f66d7a2084f18c196bd2b1fa5
rex69690 <tusharkoundal913@gmail.com>
2025-08-26 11:12:38 +0530

    First commit and Intialized Student Management system by springboot and added dependencies

---
+++ src/main/java/com/studentManagement/studentManagementSystem/repository/StudentRepository.java
@@ -0,0 +1,4 @@
+package com.studentManagement.studentManagementSystem.repository;
+
+public class StudentRepository {
+}
---
+++ src/main/java/com/studentManagement/studentManagementSystem/service/StudentService.java
@@ -0,0 +1,4 @@
+package com.studentManagement.studentManagementSystem.service;
+
+public class StudentService {
+}
